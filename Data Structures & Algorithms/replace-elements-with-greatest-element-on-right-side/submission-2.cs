f162e4e2440e536309c0600c277c98daa24c5965
Nikita Valpa <43111807+NikitaValpa@users.noreply.github.com>
2026-04-05 17:29:47 +0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-2

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] ReplaceElements(int[] arr) {
+        int maxItem = -1;
+        for(var i=0; i < arr.Length; i++)
+        {
+            for (var j = i+1; j < arr.Length; j++)
+            {
+                if (arr[j] > maxItem)
+                {
+                    maxItem = arr[j];
+                }
+            }
+
+            if (i == arr.Length-1)
+            {
+                arr[i] = -1;
+            }else
+            {
+                arr[i] = maxItem;
+                maxItem = 0;
+            }
+        }
+
+        return arr;
+    }
+}
